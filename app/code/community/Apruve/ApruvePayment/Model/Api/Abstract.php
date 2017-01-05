7ff73dd087c07733f2f265cbd328db5a63db2f68
Dave Onnen <david.onnen@gmail.com>
2017-01-05 12:59:38 -0600

    fixing urls

--- app/code/community/Apruve/ApruvePayment/Model/Api/Abstract.php
+++ app/code/community/Apruve/ApruvePayment/Model/Api/Abstract.php
@@ -218,9 +218,9 @@ abstract class Apruve_ApruvePayment_Model_Api_Abstract
     {
         $http = $secure ? 'https://' : 'http://';
         if($this->getIsTestMode()) {
-            return $http . 'localhost:3000/'; //TODO test.apruve.com
+            return $http . 'test.apruve.com/'; 
         } else {
-            return $http . 'app.apruve.com/'; //This URL works
+            return $http . 'app.apruve.com/'; 
         }
     }
 
