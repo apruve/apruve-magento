0ea18f04aca9cc87fbd220d99cfc6b674ca96b58
John Malone <john.malone@gmail.com>
2017-01-30 11:58:04 -0600

    AP-4628 - removing test code.

--- app/code/community/Apruve/ApruvePayment/Model/Api/Abstract.php
+++ app/code/community/Apruve/ApruvePayment/Model/Api/Abstract.php
@@ -218,10 +218,9 @@ abstract class Apruve_ApruvePayment_Model_Api_Abstract
     {
         $http = $secure ? 'https://' : 'http://';
         if($this->getIsTestMode()) {
-//  @todo Don't Commit This Bad Boy
-            return 'http://localhost:3000/'; #return $http . 'test.apruve.com/';
+            return $http . 'test.apruve.com/';
         } else {
-            return $http . 'app.apruve.com/'; 
+            return $http . 'app.apruve.com/';
         }
     }
 
